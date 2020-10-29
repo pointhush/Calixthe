b47e9aed00daf8c123d2cb546aba5696a4ece607
Aurélia A. / .Hush <49400908+pointhush@users.noreply.github.com>
2020-10-29 22:17:58 +0100

    Création du menu

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,18 @@
-$(function() {
-    $(".faddingheader").addClass("load");
+$(document).ready(function() {
+    $(function() {
+        $(".faddingheader").addClass("load");
+    });
+
+    function navi() {
+        $("nav").addClass("load");
+    };
+    setTimeout(navi, 1500);
+
+    $('a[href^="#"]').click(function() {
+        var id = $(this).attr("href");
+        if (id === '#') {
+            return;
+        }
+        $('html, body').animate({ scrollTop: $(id).offset().top }, 'slow');
+    });
 });
